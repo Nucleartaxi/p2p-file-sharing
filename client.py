d38929f1b09baae26259b694a66c08cce163ec3b
Alexander Shirk <alexander@truewheels.net>
2022-11-03 22:37:28 -0700

    refactored connect and disconnect methods in the sender object

--- client.py
+++ client.py
@@ -5,10 +5,15 @@ HOST = 'localhost'    # The remote host
 PORT = 50007              # The same port as used by the server
 class sender: #handles sending data 
     def __init__(self) -> None:
+        self.socket = None
         pass
     def send_data(self, str_data): #sends string data to the server
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            s.sendall(str_data.encode('utf-8'))
-            data = s.recv(1024)
-            print('Received', repr(data))
+        self.socket.sendall(str_data.encode('utf-8'))
+        data = self.socket.recv(1024)
+        print('Received', repr(data))
+    def connect(self, host, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        self.socket = s
+    def disconnect(self):
+        self.socket.close()
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ while True:
     if action == 1:
         file_to_request = input("enter file to request: ") #gets the file the user wants to request from the network
         send = sender() #initializes a sender object so we can establish a connection to the server
+        send.connect('localhost', 50007)
         send.send_data(file_to_request) #sends the file we want to request to the other program on the network 
+        send.disconnect()
     else:
         print("Invalid input. Please try again.") 
