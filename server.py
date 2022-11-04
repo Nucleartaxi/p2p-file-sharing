981d37695817dd5e549e1254a2a95d3494a58a38
Alexander Shirk <alexander@truewheels.net>
2022-11-03 22:41:15 -0700

    moved server code to a class

--- server.py
+++ server.py
@@ -3,14 +3,18 @@ import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    # while True: #server will continue to listen
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+class server:
+    def __init__(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen(1)
+            # while True: #server will continue to listen
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data: break
+                    conn.sendall(data)
+
+server1 = server()
