332c13674a0b92393dfbaf3ce48588110a83ffe8
Alexander Shirk <alexander@truewheels.net>
2022-11-29 00:17:29 -0800

    added working directory to server class and moved server py file to main directory again

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 class server:
     def __init__(self):
+        self.working_directory = "./server/"
         self.start_server()
 
     def process_chat(self, conn): 
@@ -14,7 +15,69 @@ class server:
             print(text) #print chat string to screen
             if not data: break #break if end of data
     def process_file(self, conn, filename):
-        with open(filename, "wb") as out_file: #reads the file in chunks and sends it chunk by chunk
+        with open(self.working_directory + filename, "ab") as out_file: #reads the file in chunks and sends it chunk by chunk
+            while True:
+                chunk = conn.recv(1024)
+                if not chunk: break
+                out_file.write(chunk)
+
+    def start_server(self): #starts the server. 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen(1)
+            while True: #server will continue to listen
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    header = conn.recv(1024).decode().split(" ")
+                    print("header=" + str(header))
+                    if header[0] == "CHAT":
+                        self.process_chat(conn)
+                    elif header[0] == "FILE":
+                        self.process_file(conn, header[1])
+                    # while True:
+                    #     data = conn.recv(1024)
+                    #     if not data: break
+                    #     conn.sendall(data)
+
+
+server1 = server()s s:
+            s.bind((HOST, PORT))
+            s.listen(1)
+            while True: #server will continue to listen
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    header = conn.recv(1024).decode().split(" ")
+                    print("header=" + str(header))
+                    if header[0] == "CHAT":
+                        self.process_chat(conn)
+                    elif header[0] == "FILE":
+                        self.process_file(conn, header[1])
+                    # while True:
+                    #     data = conn.recv(1024)
+                    #     if not data: break
+                    #     conn.sendall(data)
+
+
+server1 = server()# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+class server:
+    def __init__(self):
+        self.working_directory = "./server/"
+        self.start_server()
+
+    def process_chat(self, conn): 
+        while True:
+            data = conn.recv(1024) #receive data
+            text = data.decode() #decode bytes into regular string
+            print(text) #print chat string to screen
+            if not data: break #break if end of data
+    def process_file(self, conn, filename):
+        with open(self.working_directory + filename, "ab") as out_file: #reads the file in chunks and sends it chunk by chunk
             while True:
                 chunk = conn.recv(1024)
                 if not chunk: break
