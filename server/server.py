bf9982d9e6ee24b389e7f81a1478c7870dad782c
Alexander Shirk <alexander@truewheels.net>
2022-11-28 23:49:21 -0800

    refactor

--- server/server.py
+++ server/server.py
@@ -5,6 +5,22 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 class server:
     def __init__(self):
+        self.start_server()
+
+    def process_chat(self, conn): 
+        while True:
+            data = conn.recv(1024) #receive data
+            text = data.decode() #decode bytes into regular string
+            print(text) #print chat string to screen
+            if not data: break #break if end of data
+    def process_file(self, conn, filename):
+        with open(filename, "wb") as out_file: #reads the file in chunks and sends it chunk by chunk
+            while True:
+                chunk = conn.recv(1024)
+                if not chunk: break
+                out_file.write(chunk)
+
+    def start_server(self): #starts the server. 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
             s.listen(1)
@@ -22,17 +38,6 @@ class server:
                     #     data = conn.recv(1024)
                     #     if not data: break
                     #     conn.sendall(data)
-    def process_chat(self, conn): 
-        while True:
-            data = conn.recv(1024) #receive data
-            text = data.decode() #decode bytes into regular string
-            print(text) #print chat string to screen
-            if not data: break #break if end of data
-    def process_file(self, conn, filename):
-        with open(filename, "wb") as out_file: #reads the file in chunks and sends it chunk by chunk
-            while True:
-                chunk = conn.recv(1024)
-                if not chunk: break
-                out_file.write(chunk)
+
 
 server1 = server()
