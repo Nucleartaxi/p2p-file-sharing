3ed5f1007694c752c9886dbf56a3d0bd9f590c01
Alexander Shirk <alexander@truewheels.net>
2022-12-01 16:27:12 -0800

    added peer db

---
+++ peers.py
@@ -0,0 +1,34 @@
+import pickle
+
+class peer:
+    def __init__(self, name, ip, port):
+        self.name = name 
+        self.ip = ip 
+        self.port = port
+    def __repr__(self):
+        return "Name: " + self.name + " ip: " + self.ip + " port: " + str(self.port)
+
+class peer_db:
+    def __init__(self):
+        self.db = {}
+        self.load_db()
+    def add_peer(self, name, ip, port):
+        self.db[name] = peer(name, ip, port)
+        self.save_db()
+    def save_db(self):
+        with open("peer_db.pickle", "wb") as f:
+            pickle.dump(self.db, f)
+    def load_db(self):
+        with open("peer_db.pickle", "rb") as f:
+            self.db= pickle.load(f)
+    def initialize_db_with_sample_peers(self):
+        self.add_peer("Alice", "localhost", 50007)
+    def __repr__(self):
+        result = "" 
+        for x in self.db.items():
+            result += str(x[1]) + "\n"
+        return result
+
+db = peer_db()
+# db.initialize_db_with_sample_peers()
+print(db)
