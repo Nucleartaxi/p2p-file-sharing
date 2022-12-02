6c59506ffe316afa18618e48dd91c653b58738df
Alexander Shirk <alexander@truewheels.net>
2022-12-01 16:52:52 -0800

    code for UI loop in main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from client import sender
+from server import server
+from peers import peer_db
+from peers import peer
 import user_interface
 
-from server import server
 import threading
 
 test_server = server(working_directory="./server/")
@@ -9,4 +11,9 @@ thread = threading.Thread(target=test_server.start_server, args=(), daemon=True)
 thread.start()
 print("Thread started")
 
-user_interface.main_user_interface_loop() 
+
+def demo_UI():
+    db = peer_db()
+    user_interface.main_user_interface_loop(db) 
+
+demo_UI()
