d5ca887212920e8722a80db441cfab9ceb8b81f3
Alexander Shirk <alexander@truewheels.net>
2022-12-02 21:54:41 -0800

    select demo menu

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from client import sender
-from server import server
 from peers import peer_db
-from peers import peer_db, peer
-from instance import instance
+from peers import peer_db
 from user_interface import instance_user_interface
-from user import user_database, user, login_handler
+from user import user_database, login_handler
 
-class user:
-    pass
-import threading
 
 
 
 class main: #main class
     def __init__(self):
         self.peer_db = peer_db()
+    
+    def select_demo(self):
+        while True:
+            print("Which demo to run?\n0. Exit\n1. Login demo\n2. main loop no login (for testing)\n3. Mininet 10 peers demo")
+            selection = str(input())
+            if selection == "0":
+                exit() 
+            elif selection == "1":
+                self.demo_user_with_login()
+            elif selection == "2":
+                self.demo_user_loop()
+            elif selection == "3":
+                pass
 
     # def demo_UI(self):
     #     db = peer_db()
@@ -23,10 +30,11 @@ class main: #main class
         ui = instance_user_interface(peers_db=self.peer_db, peer=self.peer_db.get_peer(user)) #construct UI, run as user
         ui.start_user_interface_loop() #start UI loop
     def demo_user_with_login(self):
-        login = login_handler() 
+        user_db = user_database()
+        login = login_handler(user_db=user_db) 
         login.login_prompt(self.demo_user_loop) #login prompt that runs demo_user_loop on successful login
 
         
 
 m = main()
-m.demo_user_with_login()
+m.select_demo()
