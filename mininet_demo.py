3347962e7deb68c8dbf6cd0fc0d0333d0dc458ea
Alexander Shirk <alexander@truewheels.net>
2022-12-02 22:35:34 -0800

    test demo with localhost

--- mininet_demo.py
+++ mininet_demo.py
@@ -1,6 +1,17 @@
+from instance import instance 
+from peers import peer_db, peer 
+
 
 class mininet_demo:
     def __init__(self):
-        pass 
+        self.peer_db = peer_db()
     def start(self):
-        pass
+        instances = self.setup_instances()
+        [print(x) for x in instances]
+
+    def setup_instances(self):
+        result = [] 
+        peers = ["Bob"]
+        for name in peers:
+            result.append(instance(self.peer_db.get_peer(name)))
+        return result
